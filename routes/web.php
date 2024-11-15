343c9a71f019c9657bc3057d65a686cbbb13361f
iivaanjurado <ija0001@alu.medac.es>
2024-11-15 17:58:41 +0100

    creacion y modficacion de ritas

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/prueba', function () {
-    return view('vistaPrueba');
+Route::get('/cartelera', function () {
+    return view('cartelera');
 });

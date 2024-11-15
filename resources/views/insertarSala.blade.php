9f060fe340049f5a8b95bf42e9a0ee2420733733
Nacho Ramirez <ignacioramirezdearos05@gmail.com>
2024-11-15 17:48:44 +0100

    formulario

---
+++ resources/views/insertarSala.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <select name="" id="">
+            <option value="Super Salidos"></option>
+            <option value="8 Apellidos Vascos"></option>
+            <option value="El Orfanato"></option>
+            <option value="IT"></option>
+        </select>
+        <button>Insertar sala</button>
+    </form>
+</body>
+</html>

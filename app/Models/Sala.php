7b7222393ac95cde9cc228a79f7b54fe96a4a763
neusware <neusware@hotmail.com>
2024-11-17 19:55:22 +0100

    modelos updated
    campos no updated/created at ocultados

--- app/Models/Sala.php
+++ app/Models/Sala.php
@@ -11,6 +11,8 @@ class Sala extends Model
     
     protected $primaryKey = 'id'; 
     protected $fillable = ['pelicula'];
+    // Ocultar los campos created_at y updated_at no se incluirán en las respuestas JSON generadas por Eloquent
+    protected $hidden = ['created_at', 'updated_at'];
 
     public function asientos(){
         return $this->hasMany(Asiento::class,'idSala');

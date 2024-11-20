fc5857302a13d986994aca8dc8b437647981b1ea
iivaanjurado <ija0001@alu.medac.es>
2024-11-20 17:20:34 +0100

    modificar-vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -66,7 +66,7 @@ function mostrarAsientos(idSala) {
     cartelera.style.display = "none";
 
     //pintar el boton de atrás y el titulo de la sala donde se encuentra
-    contenido.innerHTML = `<div class="mb-4"><button id="atras" class="flex justify-center items-center ml-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600">Atrás</button><h2 class="flex justify-center items-center text-xl flex font-semibold">Asientos Disponibles en la sala ${idSala}</h2> </div>`;
+    contenido.innerHTML = `<div class="mb-4"><button id="atras" class="flex justify-center items-center ml-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600">Atrás</button><h2 class="flex justify-center items-center text-xl font-semibold">Asientos Disponibles en la sala ${idSala}</h2> </div>`;
 
     //agregar el boton de atras
     const botonAtras = document.getElementById("atras");
